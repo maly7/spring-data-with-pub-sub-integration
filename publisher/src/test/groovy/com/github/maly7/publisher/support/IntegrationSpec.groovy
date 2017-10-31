6d6c11a4f0056c22186fb48c9d0a47061b6c3826
Paul Aly <paul.aly@outlook.com>
2017-10-30 21:03:22 -0400

    adding subscriber sub module

--- publisher/src/test/groovy/com/github/maly7/publisher/support/IntegrationSpec.groovy
+++ publisher/src/test/groovy/com/github/maly7/publisher/support/IntegrationSpec.groovy
@@ -1,11 +1,11 @@
 package com.github.maly7.publisher.support
 
-import com.github.maly7.publisher.Publisher
+import com.github.maly7.publisher.PublisherApp
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import spock.lang.Specification
 
 @ActiveProfiles(profiles = 'test')
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [Publisher])
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [PublisherApp])
 class IntegrationSpec extends Specification {
 }
---
+++ subscriber/src/main/java/com/github/maly7/subscriber/SubscriberApp.java
@@ -0,0 +1,11 @@
+package com.github.maly7.subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriberApp.class, args);
+    }
+}
