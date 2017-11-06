572f8af90e1e240eea3dddd1ccd32bdf8f854368
Paul Aly <paul.aly@outlook.com>
2017-11-05 19:12:03 -0500

    Message Routing

--- subscriber/src/main/java/com/github/maly7/subscriber/config/MessagingConfig.java
+++ subscriber/src/main/java/com/github/maly7/subscriber/config/MessagingConfig.java
@@ -8,8 +8,12 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.dsl.core.Pollers;
 import org.springframework.integration.dsl.jms.Jms;
+import org.springframework.integration.router.HeaderValueRouter;
 import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.messaging.MessageChannel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MessagingConfig {
@@ -17,6 +21,11 @@ public class MessagingConfig {
     @Bean
     public MessageChannel bookIngestChannel() {
         return MessageChannels.direct("bookIngestChannel").get();
+    }
+
+    @Bean
+    public MessageChannel deleteChannel() {
+        return MessageChannels.direct("deleteChannel").get();
     }
 
     @Bean(name = PollerMetadata.DEFAULT_POLLER)
@@ -28,8 +37,21 @@ public class MessagingConfig {
     public IntegrationFlow inboundBookFlow(ActiveMQConnectionFactory jmsConnectionFactory) {
         return IntegrationFlows.from(
                 Jms.inboundAdapter(jmsConnectionFactory).destination("bookEvents"))
-                .channel(bookIngestChannel())
+                .route(ingestRouter())
                 .get();
+    }
+
+    @Bean
+    public HeaderValueRouter ingestRouter() {
+        HeaderValueRouter router = new HeaderValueRouter("EventType");
+        Map<String, String> channelMappings = new HashMap<>();
+        channelMappings.put("UPDATE", "bookIngestChannel");
+        channelMappings.put("DELETE", "deleteChannel");
+
+        router.setChannelMappings(channelMappings);
+        router.setResolutionRequired(false);
+        router.setDefaultOutputChannel(bookIngestChannel());
+        return router;
     }
 
     // For Error Routing See: https://jira.spring.io/browse/INT-4265
---
+++ subscriber/src/main/java/com/github/maly7/subscriber/service/DeleteService.java
@@ -0,0 +1,16 @@
+package com.github.maly7.subscriber.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteService {
+    private static final Logger LOG = LoggerFactory.getLogger(DeleteService.class);
+
+    @ServiceActivator(inputChannel = "deleteChannel")
+    public void delete(String book) {
+        LOG.info("Deleting Book with id: [{}]", book);
+    }
+}
