f5ad7d56821a63c61417aeaa33175ab333d09664
maly7 <paul.aly@outlook.com>
2018-01-22 21:29:45 -0500

    add TODO for later in #6

--- publisher/src/main/java/com/github/maly7/publisher/messaging/BookGateway.java
+++ publisher/src/main/java/com/github/maly7/publisher/messaging/BookGateway.java
@@ -1,12 +1,20 @@
 package com.github.maly7.publisher.messaging;
 
 import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.GatewayHeader;
 import org.springframework.integration.annotation.MessagingGateway;
 
 @MessagingGateway
 public interface BookGateway {
 
-    @Gateway(requestChannel = "bookUpdates")
+    /*
+     * TODO: use a default request channel for the entire class, but specific header values for
+     * the various methods. ie:
+     * @MessagingGateway(defaultRequestChannel = "books")
+     * @Gateway(requestChannel = "bookUpdates", headers = @GatewayHeader(name = "EventType", value = "write"))
+     */
+
+    @Gateway(requestChannel = "bookUpdates", headers = @GatewayHeader(name = "EventType", value = "write"))
     void sendUpdate(String message);
 
     @Gateway(requestChannel = "bookDeletes")
