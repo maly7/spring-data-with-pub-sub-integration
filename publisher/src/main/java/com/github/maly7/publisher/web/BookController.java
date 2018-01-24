026a6ea363add5d124d05a5a7b1e2b56c0d06a0c
maly7 <paul.aly@outlook.com>
2018-01-23 21:09:22 -0500

    embed book props

--- publisher/src/main/java/com/github/maly7/publisher/web/BookController.java
+++ publisher/src/main/java/com/github/maly7/publisher/web/BookController.java
@@ -2,6 +2,7 @@ package com.github.maly7.publisher.web;
 
 import com.github.maly7.publisher.data.BookRepository;
 import com.github.maly7.publisher.domain.Book;
+import com.github.maly7.publisher.web.resource.BookResourceAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.ResourceSupport;
--- publisher/src/main/java/com/github/maly7/publisher/web/resource/BookResourceAssembler.java
+++ publisher/src/main/java/com/github/maly7/publisher/web/resource/BookResourceAssembler.java
@@ -1,10 +1,14 @@
-package com.github.maly7.publisher.web;
+package com.github.maly7.publisher.web.resource;
 
 import com.github.maly7.publisher.domain.Book;
+import com.github.maly7.publisher.web.BookController;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -18,8 +22,13 @@ public class BookResourceAssembler extends ResourceAssemblerSupport<Book, Resour
 
     @Override
     public ResourceSupport toResource(Book entity) {
-        ResourceSupport resource = new ResourceSupport();
-        // TODO: embed props here
+        HALResource resource = new HALResource();
+
+        Map<String, String> identifiers = new HashMap<>();
+        identifiers.put("title", entity.getTitle());
+
+        resource.embed("identifiers", identifiers);
+
         resource.add(linkTo(methodOn(BookController.class).get(entity.getId())).withRel(Link.REL_SELF));
 
         return resource;
